01d0daddeb9718463c2d1569628e742a9b7b5df3
William Polinchak <william.polinchak@windstream.com>
2022-09-19 08:28:44 -0400

    Remove the old jdbc tests.

--- src/test/java/com/bpe/zookeeper/client/HelloControllerTest.java
+++ src/test/java/com/bpe/zookeeper/client/HelloControllerTest.java
@@ -9,8 +9,6 @@ import org.springframework.test.context.web.WebAppConfiguration;
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = App.class)
 @WebAppConfiguration
-// @IntegrationTest({"server.port:0",
-//         "spring.datasource.url:jdbc:h2:mem:zookeeper-client;DB_CLOSE_ON_EXIT=FALSE"})
 public class HelloControllerTest {
 
     @Test
@@ -19,20 +17,4 @@ public class HelloControllerTest {
     }
 
 
-    // @Test
-    // public void testHello() throws Exception {
-    //     when().get("/").then()
-    //             .body(is("Hello World!"));
-    // }
-
-    // @Test
-    // public void testCalc() throws Exception {
-    //     given().param("left", 100)
-    //             .param("right", 200)
-    //             .get("/calc")
-    //             .then()
-    //             .body("left", is(100))
-    //             .body("right", is(200))
-    //             .body("answer", is(300));
-    // }
 }
