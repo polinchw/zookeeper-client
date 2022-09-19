26824a7d01cfbe1e2a1990485df240fe8213e382
William Polinchak <william.polinchak@windstream.com>
2022-09-19 15:41:30 -0400

    Trying to get the tests to work.

--- src/main/java/com/bpe/zookeeper/client/App.java
+++ src/main/java/com/bpe/zookeeper/client/App.java
@@ -1,6 +1,7 @@
+package com.bpe.zookeeper.client;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
