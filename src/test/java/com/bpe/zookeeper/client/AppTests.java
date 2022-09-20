5533fbf27841d08c44659b4f22d54eb5e5913dcc
William Polinchak <william.polinchak@windstream.com>
2022-09-20 09:31:51 -0400

    The test if finally running.

--- src/test/java/com/bpe/zookeeper/client/AppTests.java
+++ src/test/java/com/bpe/zookeeper/client/AppTests.java
@@ -1,21 +1,10 @@
 package com.bpe.zookeeper.client;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.ArrayList;
+import org.junit.jupiter.api.Test;
 
-import org.apache.curator.test.TestingServer;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-@RunWith(SpringJUnit4ClassRunner.class)
+// @RunWith(SpringJUnit4ClassRunner.class)
 public class AppTests {
 
 	@Test
@@ -23,28 +12,28 @@ public class AppTests {
 		assertEquals("yes", "no");
 	}
 
-    @Test
-	public void testMethod() throws Exception {
-		org.junit.Assert.assertTrue( new ArrayList<String>().isEmpty() );
+    // @Test
+	// public void testMethod() throws Exception {
+	// 	org.junit.Assert.assertTrue( new ArrayList<String>().isEmpty() );
 
-		int zkPort = 9030;
-		TestingServer server = new TestingServer(zkPort);
+	// 	int zkPort = 9030;
+	// 	TestingServer server = new TestingServer(zkPort);
 
-		int port = 9031;
+	// 	int port = 9031;
 
-		ConfigurableApplicationContext context = new SpringApplicationBuilder(
-				App.class).run("--server.port=" + port,
-						"--spring.config.use-legacy-processing=true",
-						"--management.endpoints.web.exposure.include=*",
-						"--spring.cloud.zookeeper.connect-string=localhost:" + zkPort);
+	// 	ConfigurableApplicationContext context = new SpringApplicationBuilder(
+	// 			App.class).run("--server.port=" + port,
+	// 					"--spring.config.use-legacy-processing=true",
+	// 					"--management.endpoints.web.exposure.include=*",
+	// 					"--spring.cloud.zookeeper.connect-string=localhost:" + zkPort);
 
-		ResponseEntity<String> response = new TestRestTemplate()
-				.getForEntity("http://localhost:" + port + "/hi", String.class);
-		assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+	// 	ResponseEntity<String> response = new TestRestTemplate()
+	// 			.getForEntity("http://localhost:" + port + "/hi", String.class);
+	// 	assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
 
-		context.close();
-		server.close();
-	}
+	// 	context.close();
+	// 	server.close();
+	// }
 
 
 
